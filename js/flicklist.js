15fc7825316a8081d9df19c966340ea3fa3fa910
Matt Barger <alfers1985@gmail.com>
2016-07-28 01:53:35 +0000

    studio 1 complete

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "c665d840e21dbe2b26215f0b92b9d678" // TODO 0 put your api key here
 }
 
 
@@ -24,9 +24,10 @@ function discoverMovies(callback) {
 			api_key: api.token,
 		},
 		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-			
+
+			$.each(response['results'],function(i,v){
+				model.browseItems.push(v);
+			});
 			// TODO 2
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
 			
@@ -44,20 +45,31 @@ function discoverMovies(callback) {
 function render() {
   // TODO 7
   // clear everything from both lists
-  
+  	$("ul").empty();
   // TODO 6
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
-  
+  $.each(model.watchlistItems,function(movie,data) {
+			var mymovie = $('<li></li>').append($('<p></p>').text(data));
+			$("#section-watchlist ul").append(mymovie);
+  });	
   // for each movie on the current browse list, 
-  model.browseItems.forEach(function(movie) {
+  $.each(model.browseItems,function(movie,data) {
 		// TODO 3
 		// insert a list item into the <ul> in the browse section
-		
+			var mymovie = $('<li></li>').append($('<p></p>').text(data.title));
+		//	$("#section-browse ul").append(mymovie);
 		// TODO 4
 		// the list item should include a button that says "Add to Watchlist"
-		
+			var mybutton = $('<button></button>').text("Add to Watchlist");
+			$("#section-browse ul").append(mymovie).append(mybutton);
 		// TODO 5
 		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+  		mybutton.click(function(e){
+  			model.watchlistItems.push(data.title);
+  			render();
+  		});
+  	
+  	
   });
   
 }
